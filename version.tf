5ca55dd6d7f8e3fc3d81b6dd04f9304956576ff7
Alan Giovanni <alan1884@hotmail.com>
2023-04-29 12:46:43 -0300

    terraform fmt

--- version.tf
+++ version.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = "~> 1.3.7"
-  
+
   required_providers {
     datadog = {
-      source = "DataDog/datadog"
+      source  = "DataDog/datadog"
       version = "~> 3.24"
     }
   }
