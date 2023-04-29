781227516fb400f8c0bb3e6e56489b713eb1aa71
Alan Giovanni <alan1884@hotmail.com>
2023-04-29 12:46:13 -0300

    Ajuste nos comentários

--- monitor-api-hgweather.tf
+++ monitor-api-hgweather.tf
@@ -1,17 +1,17 @@
 # Example Usage (Synthetics API test)
-# Create a new Datadog Synthetics API/HTTP test on https://www.example.org
+
 resource "datadog_synthetics_test" "test_api" {
   type    = "api"
   subtype = "http"
   request_definition {
-    method = "GET"
+    method  = "GET"
     timeout = 10
-    url    = "https://api.hgbrasil.com/weather?woeid=455872"
+    url     = "https://api.hgbrasil.com/weather?woeid=455872"
   }
   request_headers = {
 
   }
-
+  # Validações do Monitoramento
   assertion {
     type     = "statusCode"
     operator = "is"
@@ -28,17 +28,17 @@ resource "datadog_synthetics_test" "test_api" {
     type     = "body"
     operator = "validatesJSONPath"
     targetjsonpath {
-      jsonpath = "$.results.city_name"
-      operator = "is"
+      jsonpath    = "$.results.city_name"
+      operator    = "is"
       targetvalue = "João Pessoa"
     }
   }
 
   locations = ["aws:us-west-1"]
   options_list {
-    tick_every = 300
-    min_location_failed = 1
-    min_failure_duration = 600 #Segundos, tempo de alerta para enviar uma notificação
+    tick_every           = 300
+    min_location_failed  = 1
+    min_failure_duration = 600
 
     retry {
       count    = 2
